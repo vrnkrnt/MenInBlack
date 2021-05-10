192f1e27f16c591a2bfdb298524e5a016ba3a910
Karin Mäki-Kala <k.maki-kala@live.se>
2021-05-10 15:58:20 +0200

    Karins alienklass

--- Alien.java
+++ Alien.java
@@ -5,10 +5,58 @@
  */
 package MenInBlack;
 
+import java.util.Date;
+
 /**
  *
  * @author veron
  */
-public class Alien {
+class Alien {
+    private int id, plats, ansvarigAgent;
+    private String namn;
+    private String telefonnummer;
+    private Date datum;
+    private String losenord;
+ 
+    public Alien(int id, int plats, int ansvarigAgent, String namn, 
+            String telefonnummer, Date datum, String losenord){
+    this.id = id;
+    this.plats = plats;
+    this.ansvarigAgent = ansvarigAgent;
+    this.namn = namn;
+    this.telefonnummer = telefonnummer;
+    this.datum = datum;
+    this.losenord = losenord;
+    }
     
+    public int getId(){
+        return id;
+    }
+       
+    public int getPlats(){
+        return plats;
+    }
+    
+    public int getAnsvarigAgent(){
+        return ansvarigAgent;
+    }
+    
+    public String getNamn(){
+        return namn;
+    }
+    
+    public String getTelefonnummer(){
+    return telefonnummer;
+    }
+    
+    public Date getDatum(){
+        return datum;
+    }
+    
+    public String getLosenord(){
+        return losenord;
+    }
 }
+
+
+
