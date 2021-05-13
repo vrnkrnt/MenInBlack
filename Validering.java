92db4a0aa2d9f3ed4f99b0bf3bdd3d49654ef07a
Veronika Ranta <veronika@ranta.com>
2021-05-13 10:34:03 +0200

    Added Validering.java file

---
+++ Validering.java
@@ -0,0 +1,58 @@
+package MenInBlack;
+
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Emil Lager
+ * @author Josefin Olsson
+ * @author Karin Mäki-Kala
+ * @author Veronika Ranta
+ * 
+ */
+
+public class Validering {
+    
+    public static boolean testFaltHarVarde(JTextField rutaAttKolla)
+    {
+        boolean resultat = true;
+        
+        if(rutaAttKolla.getText().isEmpty())
+        {
+            JOptionPane.showMessageDialog(null, "Inmatningsrutan är tom.");
+            resultat = false;
+            rutaAttKolla.requestFocus();
+        }
+        
+        return resultat;
+    }
+    
+    public static boolean isHeltal(JTextField rutaAttKolla)
+    {
+        boolean resultat = true;
+        
+        try
+        {
+            String inStrang = rutaAttKolla.getText();
+            Integer.parseInt(inStrang);
+            rutaAttKolla.requestFocus();
+        }
+        
+        catch(NumberFormatException e)
+        {
+            JOptionPane.showMessageDialog(null, "Var god ange ett heltal");
+            resultat = false;
+        }
+        
+        return resultat;
+    }
+    
+}
