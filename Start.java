83480505d95e7e57b54b4d5dea8982458bcf0f63
Veronika Ranta <veronika@ranta.com>
2021-05-17 18:51:55 +0200

    Added code to RegUtrustningWin

--- Start.java
+++ Start.java
@@ -28,7 +28,7 @@ public class Start {
             String agentNamn = "Agent Z";
             String agentID = idb.fetchSingle("select agent_id from agent where namn = '" + agentNamn + "'");
             System.out.println(agentID);
-           
+            new RegUtrustningWin (idb).setVisible(true);  
         }
         
         
